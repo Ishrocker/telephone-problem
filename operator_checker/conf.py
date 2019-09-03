f463b8c0c203fe266de38414044ff4bef66d883b
Lucas Scott <lucas.scott@starstable.com>
2019-09-03 18:29:32 +0200

    typo

--- operator_checker/conf.py
+++ operator_checker/conf.py
@@ -1,7 +1,7 @@
 
 class App:
     description = """
-    Find the cheapes operator for a phone call!
+    Find the cheapest operator for a phone call!
     
     If no match is found, nothing will be outputed to console.
     """
